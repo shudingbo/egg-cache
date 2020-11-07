e6c424e91eb0894e6d504d4eeb38502469fb0af4
shudingbo <shudingbo@qq.com>
2020-11-07 19:36:40 +0800

    update deps,bump version

--- app.js
+++ app.js
@@ -2,21 +2,21 @@
 const pro = require('process');
 const Store = require('./lib/store');
 
-async function cacheSync (app,data) {
+async function cacheSync (app, data) {
   if (data.pid === pro.pid) {
     return;
   }
 
   switch (data.act) {
-  case 0: 
-    if( data.isMemSync === true ){
+  case 0:
+    if (data.isMemSync === true) {
       await app.cache.store('memory').del(data.name, data.isMemSync);
     }
-  break;
+    break;
   case 1:
-      if( data.options.isMemSync ){
-        await app.cache.store('memory').set( data.name,data.value,data.expire,data.options );
-      }
+    if (data.options.isMemSync) {
+      await app.cache.store('memory').set(data.name, data.value, data.expire, data.options);
+    }
     break;
   default: break;
   }
@@ -27,7 +27,7 @@ module.exports = app => {
   app.cache = require('./lib/cache')(app);
   process.on('message', msg => {
     if (msg.action === Store.syncKey) {
-      cacheSync( app, msg.data );
+      cacheSync(app, msg.data);
     }
   });
 };
